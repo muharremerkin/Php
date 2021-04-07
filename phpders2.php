efe4620b06e368578bbc5cb27504aae618c0e5b8
muharremerkin <79638688+muharremerkin@users.noreply.github.com>
2021-04-08 00:48:52 +0300

    Add files via upload

---
+++ phpders2.php
@@ -0,0 +1,28 @@
+phpders2.php
+<html> <head><!-- Html Head Kısmı --> <!-- Shift+Alt+A kısayolu ile burası eklenebilir --> 
+
+<title>Web Proglamlama Dersi</title> 
+<meta charset="UTF-8"> 
+<meta name="keywords" content="Web,Php,Asp.Net,Programlama"> 
+<meta name="description" content="Deneme,tasarım,proglama"> 
+<meta name="author" content="Yazılım"> 
+</head> 
+
+<body>
+
+<h4>Kişisel Bilgiler - Ödevi </h4> 
+
+<!-- HTML Açıklama Satırıdır. --> 
+<?php /* Php Açıklama Satırları*/ 
+
+ echo "<table border='2' width='500px'>";echo "<tr>";echo "<th>Üniversite</th>";echo "<th>MYO</th>";echo "<th>Ad-Soyad</th>";
+ echo "<td></td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>Muharrem ERKİN</td>";
+ echo "<td>206001032</td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>Alperen KOÇDEVECİ</td>"; 
+ echo "<td>206001044</td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>İlayda MUTLU</td>"; 
+ echo "<td>206001012</td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>Mecit MENEKŞE</td>"; 
+ echo "<td>206001026</td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>Yusuf AKKOBAK</td>";
+ echo "<td>206001012</td>"; echo "</tr>"; echo "<tr>"; echo "<td>ADÜ</td>"; echo "<td>AYMES</td>"; echo "<td>Ferit DARCAN</td>";
+ echo "<td>206001060</td>"; echo "</tr>"; echo "</table>";
+ 
+ 
+ ?> </body> </html>

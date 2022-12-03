3f08c40521e2bd1f8a42020b9d6f71d87ce6d714
muharremerkin <79638688+muharremerkin@users.noreply.github.com>
2022-12-03 15:44:53 +0300

    Add files via upload

---
+++ phpders1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Web, Php, Mvc, Programlama">
+    <meta name="descrption" content="Hepta Firması Yazılım Alanında 2002 yılından beri hizmet vermektedir ">
+    <meta name="author" content="Hepta Yazılım">
+    <title>Hepta</title>
+
+</head>
+<body>
+    <h4>Php Programlama</h4>
+
+
+    <?php>
+
+    echo "<h3>Değişken Tanımlama Örnekleri</h3>";
+    $isim"Muharrem";
+    $soyisim"Erkin";
+    $no"206001032";
+    $üni"Aydın Adnan Menderes Üniversitesi Meslek Yüksek Okulu";
+
+    echo $isim;
+
+
+    ?>
+
+
+
+</body>
+</html>
